7772810e820bdff5f7e0a33718357a263282bcb9
Alexander Turiak <alex@atwix.com>
2011-12-22 17:04:44 +0200

    Moved code to community pool

--- app/code/community/Atwix/ClearCache/controllers/Adminhtml/DoitController.php
+++ app/code/community/Atwix/ClearCache/controllers/Adminhtml/DoitController.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Clear Cache module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Atwox
+ * @package    Atwix_ClearCache
+ * @copyright  Copyright (c) 2011 Atwix (http://www.atwix.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Alexander Turiak <alex@atwix.com>
+ */
+
 class Atwix_ClearCache_Adminhtml_DoitController extends Mage_Adminhtml_Controller_Action
 {
     public function indexAction()
