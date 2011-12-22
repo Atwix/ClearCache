cd5755a70068bda57c91b097b31b3a2514cb6ed9
Alexander Turiak <alex@atwix.com>
2011-12-22 16:26:52 +0200

    Fix for a misprint

--- app/code/local/Atwix/ClearCache/controllers/Adminhtml/DoitController.php
+++ app/code/local/Atwix/ClearCache/controllers/Adminhtml/DoitController.php
@@ -10,7 +10,7 @@ class Atwix_ClearCache_Adminhtml_DoitController extends Mage_Adminhtml_Controlle
             Mage::dispatchEvent('adminhtml_cache_refresh_type', array('type' => $cacheType));
         }
         Mage::getSingleton('adminhtml/session')->addSuccess(
-            Mage::helper('adminhtml')->__('Cache was cleared.');
+            Mage::helper('adminhtml')->__('Cache was cleared.')
         );
         $this->_redirectUrl($this->_getRefererUrl());
     }
